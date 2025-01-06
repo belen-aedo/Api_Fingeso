5e3345c0113193a339557db19bfc9ae60383a93a
Diego9028 <101033049+Diego9028@users.noreply.github.com>
2025-01-05 22:40:35 -0300

    Se elimina este backend pues no se utiliza

--- backend-v1.6/backend/src/main/java/com/example/backend/Repository/SucursalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repository;
-
-import com.example.backend.Entity.Sucursal;
-
-public interface SucursalRepository {
-    Sucursal findById(long id);
-    Sucursal findByNombreSucursal(String nombre);
-
-}
--- backend-v1.6/backend/src/main/java/com/example/backend/Repository/VehiculoReferenciaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.Repository;
-
-import com.example.backend.Entity.VehiculoReferencia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehiculoReferenciaRepository extends JpaRepository<VehiculoReferencia, Long> {
-}
-
